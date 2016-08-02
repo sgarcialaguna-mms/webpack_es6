1a26fd39965e87938aa1da6ccdd6e65c38689ac6
santa <sgarcialaguna@googlemail.com>
2016-08-02 20:37:41 +0200

    Actually add webpack config (durr.)

---
+++ angularjs_require/webpack.config.js
@@ -0,0 +1,28 @@
+/* jshint node:true */
+
+var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'js'),
+
+    entry: './app.js',
+
+    output: {
+        path: path.join(__dirname, 'js', 'bundles'),
+        filename: 'bundle.js',
+        publicPath: '/static/bundles/'
+    },
+
+    resolve: {
+        root: [path.join(__dirname, 'js')]
+    },
+    
+    module: {
+        loaders: [{
+            test: require.resolve('angular'),
+            loader: 'exports?angular'
+        }]
+    },
+
+    devtool: 'eval'
+};
